8a11307228cae0394220b9f1f0c9673e5aec218a
lotsuki <otsuki.lindsey@gmail.com>
2018-10-25 20:18:42 -0700

    update jest and enzyme

--- spec/spec.js
+++ spec/spec.js
@@ -4,9 +4,21 @@ import { mount, shallow, render } from 'enzyme';
 import App from '../client/src/components/App.jsx';
 
 
-//let wrapper = mount(<App />);
-  test('App is an instance of App', () => {
-    shallow(<App />)
-    // expect(wrapper.find('.greeting')).toHaveClassName('greeting');
+describe('App component', () => {
+  test('should shallow correctly', () => {
+    expect(shallow(<App />)).toMatchSnapshot()
   });
+  test('should mount correctly', () => {
+    expect(mount(<App />)).toMatchSnapshot()
+  });
+  test('should render correctly', () => {
+    expect(render(<App />)).toMatchSnapshot()
+  });
+  test('should output text', () => {
+    const wrapper = shallow(<App />);
+    const text = wrapper.find('.greeting').text();
+    expect(text).toEqual('Hi...my name is Lindsey')
+
+  });
+});
 
