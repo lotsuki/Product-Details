9ea5081873041e9953e919a2f03c942c20a15a4f
lotsuki <otsuki.lindsey@gmail.com>
2018-10-26 14:39:44 -0700

    Created mock data

--- database/seed.js
+++ database/seed.js
@@ -1,6 +1,37 @@
-const db = require('./index.js');
-const Product = require('./Product.js');
+//const db = require('./index.js');
+//const Product = require('./Product.js');
+const faker = require('faker');
 
-const sampleProducts = [
 
-];
+var makeMockData = () => {
+  var products = [];
+  for (var i = 1; i < 100; i++) {
+    products.push({
+      _id: i,
+      name: faker.commerce.productName(),
+      reviews: {
+        title: faker.lorem.sentence(),
+        review: faker.lorem.sentences(),
+        rating: faker.random.number({min: 1, max: 5}),
+        recommend: faker.random.boolean()
+      },
+      itemNum: i,
+      price: faker.commerce.price(50, 500),
+      color: faker.commerce.color()
+    });
+  }
+  return products;
+}
+
+
+//console.log(makeMockData())
+
+const insertSampleProducts = () => {
+  Product.create(makeMockData())
+  .then(() => db.disconnect())
+};
+
+insertSampleProducts();
+
+
+
